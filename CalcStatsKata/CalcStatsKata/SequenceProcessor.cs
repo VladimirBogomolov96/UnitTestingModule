47b2d9db4425a5f9f31c60a40b2a3b8f0f315fed
VladimirBogomolov96 <vladimir.bogomolov96@gmail.com>
2022-01-28 22:51:27 +0300

    Add SequenceProcessor implementation

--- CalcStatsKata/CalcStatsKata/SequenceProcessor.cs
+++ CalcStatsKata/CalcStatsKata/SequenceProcessor.cs
@@ -10,7 +10,38 @@ namespace CalcStatsKata
     {
         public SequenceProcessingStatistics ProcessSequence(IEnumerable<int> sequence)
         {
-            throw new NotImplementedException();
+            if (sequence is null)
+            {
+                throw new ArgumentNullException(nameof(sequence), "sequence should be not null");
+            }
+
+            if (sequence.Count() is 0)
+            {
+                throw new ArgumentException("sequence should has at least 1 item", nameof(sequence));
+            }
+
+            SequenceProcessingStatistics result = new SequenceProcessingStatistics() { MaxValue = int.MinValue, MinValue = int.MaxValue };
+            double sumOfElements = 0;
+
+            foreach (int num in sequence)
+            {
+                if (num > result.MaxValue)
+                {
+                    result.MaxValue = num;
+                }
+
+                if (num < result.MinValue)
+                {
+                    result.MinValue = num;
+                }
+
+                result.NumberOfElements++;
+                sumOfElements += num;
+            }
+
+            result.AverageValue = sumOfElements / result.NumberOfElements;
+
+            return result;
         }
     }
 }
