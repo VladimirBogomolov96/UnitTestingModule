e4626552418e08bb755f2bd2f193f91be8e1395c
VladimirBogomolov96 <vladimir.bogomolov96@gmail.com>
2022-01-28 22:50:52 +0300

    Fix SequenceProcessor tests

--- CalcStatsKata/CalcStatsKata.Tests/SequenceProcessorStatisticsTests.cs
+++ CalcStatsKata/CalcStatsKata.Tests/SequenceProcessorStatisticsTests.cs
@@ -38,9 +38,9 @@ namespace CalcStatsKata.Tests
         }
 
         [TestMethod]
-        [DataRow(0, 5, 0, 3, 4.5)]
+        [DataRow(0, 5, 0, 3, 3)]
         [DataRow(1, 2, -7, 2, -2.5)]
-        [DataRow(2, 234123512, -956459687, 7, -151332709)]
+        [DataRow(2, 234123512, -956459687, 7, -151332708)]
         [DataRow(3, 0, 0, 1, 0)]
         [DataRow(4, 2147483647, -2147483648, 2, -0.5)]
         public void ProcessSequence_ValidSequence_ReturnsCorrectStatistics(int testCaseNumber, int expectedMaxValue, int expectedMinValue, int expectedNumberOfElements, double expectedAverageValue)
@@ -52,7 +52,7 @@ namespace CalcStatsKata.Tests
             Assert.AreEqual(expectedMaxValue, statistics.MaxValue);
             Assert.AreEqual(expectedMinValue, statistics.MinValue);
             Assert.AreEqual(expectedNumberOfElements, statistics.NumberOfElements);
-            Assert.IsTrue((statistics.AverageValue - expectedAverageValue) < 0.0001);
+            Assert.IsTrue(Math.Abs(statistics.AverageValue - expectedAverageValue) < 0.0001);
         }
     }
 }
