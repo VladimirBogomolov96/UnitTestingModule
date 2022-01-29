c24feaa93974cf6f11690f2ec6291c5ce24f25b5
VladimirBogomolov96 <vladimir.bogomolov96@gmail.com>
2022-01-29 14:43:14 +0300

    Add tests for StringSumUtility

--- StringSumKata/StringSumKata.Tests/StringSumUtilityTests.cs
+++ StringSumKata/StringSumKata.Tests/StringSumUtilityTests.cs
@@ -6,5 +6,25 @@ namespace StringSumKata.Tests
     [TestClass]
     public class StringSumUtilityTests
     {
+        private StringSumUtility stringSumUtility = new StringSumUtility();
+
+        [TestMethod]
+        [DataRow("", "", "0")]
+        [DataRow(null, null, "0")]
+        [DataRow("", "22", "22")]
+        [DataRow("33", null, "33")]
+        [DataRow("dsfa", "hfdghd", "0")]
+        [DataRow("gdfgsg", "12345", "12345")]
+        [DataRow("sdffs324", "66", "66")]
+        [DataRow("-754", "-111", "-865")]
+        [DataRow("3244.6", "34,533", "0")]
+        public void Sum_ValidInput_ReturnsCorrectResult(string num1, string num2, string expectedResult)
+        {
+            // Act
+            string result = this.stringSumUtility.Sum(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
