882d988295adef716999f96973af857b9dbf6929
VladimirBogomolov96 <vladimir.bogomolov96@gmail.com>
2022-01-29 14:52:52 +0300

    Implement StringSumUtility

--- StringSumKata/StringSumKata/StringSumUtility.cs
+++ StringSumKata/StringSumKata/StringSumUtility.cs
@@ -10,7 +10,18 @@ namespace StringSumKata
     {
         public string Sum(string num1, string num2)
         {
-            throw new NotImplementedException();
+            int number1, number2;
+            if (!int.TryParse(num1, out number1))
+            {
+                number1 = 0;
+            }
+
+            if (!int.TryParse(num2, out number2))
+            {
+                number2 = 0;
+            }
+
+            return (number1 + number2).ToString();
         }
     }
 }
