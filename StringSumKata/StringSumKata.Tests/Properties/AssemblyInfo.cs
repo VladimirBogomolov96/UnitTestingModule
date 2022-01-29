b48afbadcde251589b5e3d61bb2882d73e6d35b9
VladimirBogomolov96 <vladimir.bogomolov96@gmail.com>
2022-01-29 14:09:45 +0300

    Add initial solution structure for String Sum Kata

---
+++ StringSumKata/StringSumKata.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringSumKata.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("StringSumKata.Tests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b6e76966-a65c-483c-b815-91e813daf1f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringSumKata/StringSumKata/StringSumUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSumKata
+{
+    public class StringSumUtility
+    {
+        public string Sum(string num1, string num2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
