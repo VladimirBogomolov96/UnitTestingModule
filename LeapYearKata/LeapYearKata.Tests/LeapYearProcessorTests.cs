3529274b0cb3152d52346bd7175083af7533f612
VladimirBogomolov96 <vladimir.bogomolov96@gmail.com>
2022-01-29 13:28:11 +0300

    Add tests for LeapYearProcessor

--- LeapYearKata/LeapYearKata.Tests/LeapYearProcessorTests.cs
+++ LeapYearKata/LeapYearKata.Tests/LeapYearProcessorTests.cs
@@ -6,5 +6,24 @@ namespace LeapYearKata.Tests
     [TestClass]
     public class LeapYearProcessorTests
     {
+        private LeapYearProcessor processor = new LeapYearProcessor();
+
+        [TestMethod]
+        [DataRow(0, true)]
+        [DataRow(2000, true)]
+        [DataRow(2001, false)]
+        [DataRow(2100, false)]
+        [DataRow(-100, false)]
+        [DataRow(-104, true)]
+        [DataRow(2147483647, false)]
+        [DataRow(-2147483648, true)]
+        public void IsLeapYear_ValidValues_ReturnsCorrectResult(int year, bool expectedResult)
+        {
+            // Act
+            bool result = this.processor.IsLeapYear(year);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
